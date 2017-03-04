152592a9b574ac6236317d8e507dd20f4255ff83
Gabor <csgabor14@gmail.com>
2017-03-03 20:38:41 -0500

    Gabor out

--- probability.py
+++ probability.py
@@ -4,11 +4,20 @@ Created on Thu Mar  2 20:10:02 2017
 
 @author: Gabor
 """
+import re
+
 class Probab():
     def __init__(self):
         self.words = {}
         self.transitions = {}
         self.prevPOS = 'Start'
+        self.unknown = {}
+        self.ing = {}
+        self.ly = {}
+        self.s_ending = {}
+        self.er = {}
+        self.hyphenated = {}
+        self.numbers = {}
     
     def readIntoTable(self, file):
         with open(file, "r") as inputfile:
@@ -17,9 +26,11 @@ class Probab():
                 if (element[0] == ''):
                     continue
                 self.addElem(element)        
-        inputfile.close() 
+        inputfile.close()        
+        self.preProcOOV()
+        self.calcProb()        
         
-    #adding an element to the dictionary
+    #adding an element to the dictionary to count its frequency
     def addElem(self, element):
         #counting words
         if (element[1] in self.words):
@@ -41,27 +52,79 @@ class Probab():
         
         self.prevPOS = element[1]
     
-    #calculates probability from frequency  
-    def calcProb(self):
-        self.loopProb(self.words)
-        self.loopProb(self.transitions)
-    def loopProb(self, dict):
-        for outer_key in dict:
-            total = 0
-            for key in dict[outer_key]:
-                total += dict[outer_key][key]
-            for key in dict[outer_key]:
-                dict[outer_key][key] = dict[outer_key][key]/total
-    
-    #get functions    
-    def getTran(self, tran):
-        return self.transitions[tran]
-    def getWord(self, word):
-        return self.words[word]
-    
     #unique strategy to deal with OOV
-    def OOV(self):
-        print('shit')
+    #words that only appear once get put into special categories
+    def preProcOOV(self):        
+        for tag in self.words:
+            for word in self.words[tag]:
+                if (self.words[tag][word] < 2):                 
+                    if (word[-3:] == 'ing'):
+                        self.addToDict(self.ing, tag)
+                    elif (word[-1] == 's'):
+                        self.addToDict(self.s_ending, tag)
+                    elif (word[-2:] == 'ly'):
+                        self.addToDict(self.ly, tag)
+                    elif (word[-2:] == 'er'):
+                        self.addToDict(self.er, tag)
+                    elif (re.search('\d', word)):
+                        self.addToDict(self.numbers, tag)
+                    elif (re.search('-', word)):
+                        self.addToDict(self.hyphenated, tag)
+                    else:
+                        self.addToDict(self.unknown, tag)             
+    
+    #converts all dictionaries to probability from frequency  
+    def calcProb(self):       
+        for dictionary in [self.words, self.transitions]: 
+            for outer_key in dictionary:
+                total = 0
+                for key in dictionary[outer_key]:
+                    total += dictionary[outer_key][key]
+                for key in dictionary[outer_key]:
+                    dictionary[outer_key][key] = dictionary[outer_key][key]/total
+        for dictionary in [self.unknown, self.ing, self.ly, self.s_ending, self.er, self.hyphenated, self.numbers]:
+            total = 0
+            for tag in dictionary:
+                total += dictionary[tag]
+            for tag in dictionary:
+                dictionary[tag] = dictionary[tag]/total
+    
+    #get probability function, if word is unknown, gets special treatment    
+    def getProb(self, prevState, state, word):
+        if (word in self.words[state]):
+            word_prob = self.words[state][word]
+        #out of vocab word
+        elif (word[-3:] == 'ing'):
+            if state in self.ing:
+                word_prob = self.ing[state] 
+        elif (word[-1] == 's'):
+            if state in self.s_ending:
+                word_prob == self.s_ending[state] 
+        elif (word[-2:] == 'ly'):
+            if state in self.ly:
+                word_prob ==self.ly[state] 
+        elif (word[-2:] == 'er'):
+            if state in self.er:
+                word_prob = self.er[state] 
+        elif (re.search('\d', word)):
+            if state in self.numbers:
+                word_prob = self.numbers[state] 
+        elif (re.search('-', word)):
+            if state in self.hyphenated:
+                word_prob = self.hyphenated[state] 
+        elif (state in self.unknown):
+            word_prob = self.unknown[state]
+        else:
+            word_prob = 0.000000000001
+            
+        return self.transitions[prevState][state] * word_prob
+    
+    #util function to add something to a dictionary
+    def addToDict(self, dictionary, tag):
+        if tag in dictionary:
+            dictionary[tag] += 1
+        else:
+            dictionary[tag] = 1
         
 
 
--- transducer.py
+++ transducer.py
@@ -7,8 +7,20 @@ Created on Fri Mar  3 15:55:15 2017
 import probability
 
 processor = probability.Probab() 
-processor.readIntoTable("WSJ_24.pos")       
-processor.calcProb() 
+processor.readIntoTable("WSJ_24.pos") 
 
-print(processor.getTran('NNS'))
-print(processor.getWord('NNS'))
+outputfile = open('workfile.txt', 'w')
+for tag in processor.words:
+    for word in processor.words[tag]:
+        outputfile.write(tag + ':\t' + word + '\t\t' + str(processor.words[tag][word]) + '\n')
+outputfile.write(tag + ':' + word + '\n')
+outputfile.close()
+
+"""
+Use processor.getProb(<prev-state>, <current-state>, <word>) to get: 
+    the probability of transition from prev to current state
+    times
+    the probability of current state's manifestation as the word 
+    IF word is new to the dictionary, special rules determine its probability
+"""
+print(processor.getProb('NNS', 'NN', 'getuling'))
